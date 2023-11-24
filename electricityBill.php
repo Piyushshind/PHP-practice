fb4d2025e043bb08d8e42f8a68266253b6562e9a
Piyushshind <piyushshinde.199@gmail.com>
2023-11-24 19:56:08 +0530

    bill

--- electricityBill.php
+++ electricityBill.php
@@ -5,22 +5,36 @@
 </head>
 <body>
     <?php
-    function calculateBill ($units){
+    function calculateBill (){
+        $units = $_POST['unit'];
+         $totalbill = 0.00;
         if($units <=50){
-            return $units*3.50;
+            $totalbill= $units*3.50;
         }
-        if($units <=100){
-            return $units*4;
+        elseif($units <=100){
+            $totalbill= $units*4;
         }
-        if($units >100 && $units<250){
-            return $units*5.20;
+        elseif($units >100 && $units<250){
+            $totalbill= $units*5.20;
         }
-        if($units >250){
-            return $units*6.50;
+        elseif($units >250){
+            $totalbill= $units*6.50;
+        }else{
+             echo "Enter valid units";
         }
+        echo "your unites are ". $units ." and Total Bill is " . $totalbill;
     }
-    $value = calculateBill(148);
-    echo $value;
+   
+    ?>
+
+    <form method="post" action="">
+        <input name="unit" type="number" />
+        <input type="submit" name="submit" value="submit"/>
+    </form>
+    <?php
+    if(isset($_POST['submit'])){
+        calculateBill();
+    }
     ?>
 </body>
 </html>
